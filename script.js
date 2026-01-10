775ce1fbfd24b7cee493ae140cc52f16c313bb0a
87V4 <young.delo.or.yd@gmail.com>
2026-01-10 03:21:08 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simple click animation feedback
-document.querySelectorAll(".btn").forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.style.transform = "scale(0.96)";
-    setTimeout(() => btn.style.transform = "", 120);
-  });
-});
